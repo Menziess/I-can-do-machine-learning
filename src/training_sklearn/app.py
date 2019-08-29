c8043ef2ce05bb93a17d081e650987c42cd0a09c
Menziess <stefan_schenk@hotmail.com>
2019-08-29 23:48:41 +0200

    Predictions work.

--- src/training_sklearn/app.py
+++ src/training_sklearn/app.py
@@ -1,12 +1,16 @@
 from sklearn import datasets
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+from PIL import Image
+from io import BytesIO
 
-import imageio
+import base64
 import pickle
+
 
 def load_model(path):
     with open(path, 'rb') as f:
         return pickle.load(f)
+
 
 digits = datasets.load_digits()
 model = load_model('./src/models/clf.pickle')
@@ -14,8 +18,8 @@ model = load_model('./src/models/clf.pickle')
 
 # Boot app
 app = Flask(__name__,
-    static_folder="./frontend/static",
-    template_folder="./frontend")
+            static_folder="./frontend/static",
+            template_folder="./frontend")
 
 
 # Handle generic 404
@@ -27,6 +31,7 @@ def not_found(e):
         'detail': e.description,
     })
 
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -34,10 +39,31 @@ def index():
 
 @app.route("/predict", methods=['POST'])
 def predict_image():
-    im = imageio.imread('astronaut.png')
-    im.shape  # im is a numpy array
-    (512, 512, 3)
-    imageio.imwrite('imageio:astronaut-gray.jpg', im[:, :, 0])
+    import re
+    import numpy as np
+    data = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD/2wBDAQMDAwQDBAgEBAgQCwkLEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBD/wAARCAAIAAgDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAn/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/AJVAA//Z'
+    data = request.get_data().decode("utf-8")
+    image_data = re.sub('^data:image/.+;base64,', '', data)
+    decoded_data = base64.b64decode(image_data)
+    im = Image.open(BytesIO(decoded_data))
+    im = im.convert('L')
+    matrix = np.array(im) / 20
+    flat = matrix.flatten()
+
+    global model
+    return jsonify({
+        'prediction': int(model.predict([flat])[0]),
+        'data': str(flat)
+    })
+
+    # data = request.get_data()
+    # decoded = base64.b64decode(data)
+    # Image.open(decoded)
+
+    # im = imageio.imread('astronaut.png')
+    # im.shape  # im is a numpy array
+    # (512, 512, 3)
+    # imageio.imwrite('imageio:astronaut-gray.jpg', im[:, :, 0])
 
 
 @app.route("/predict/<int:i>/")
@@ -47,7 +73,6 @@ def predict_index(i):
         'prediction': int(model.predict(digits.data[i:])[0]),
         'actual': int(digits.target[i])
     })
-
 
 
 if __name__ == "__main__":
--- src/training_sklearn/frontend/static/main.js
+++ src/training_sklearn/frontend/static/main.js
@@ -1,53 +1,61 @@
+$(function () {
 
-var canvas = document.getElementById("canvas");
-var classify = document.getElementById("classify");
-var reset = document.getElementById("reset");
+  var canvas = document.getElementById("canvas");
+  var classify = document.getElementById("classify");
+  var reset = document.getElementById("reset");
 
-var ctx = canvas.getContext('2d');
-ctx.lineCap = 'round';
-
-var WIDTH = 8;
-var HEIGHT = 8;
+  var ctx = canvas.getContext('2d');
+  ctx.lineCap = 'round';
 
 
-var mouseIsDown = false;
+  var mouseIsDown = false;
 
-function draw(x, y) {
-  ctx.strokeStyle = 'black';
-  ctx.lineTo(x, y);
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.fill();
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-}
+  function draw(x, y) {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  }
 
-canvas.onmousedown = function (e) {
-  mouseIsDown = true;
-}
+  canvas.onmousedown = function (e) {
+    mouseIsDown = true;
+  }
 
-canvas.onmouseup = function (e) {
-  if (mouseIsDown) mouseClick(e);
-  mouseIsDown = false;
-}
+  canvas.onmouseup = function (e) {
+    if (mouseIsDown) mouseClick(e);
+    mouseIsDown = false;
+  }
 
-canvas.onmousemove = function (e) {
-  if (!mouseIsDown) return;
-  var rect = canvas.getBoundingClientRect();
-  var x = (e.pageX - window.pageXOffset - rect.x) / 20;
-  var y = (e.pageY - window.pageYOffset - rect.y) / 20;
-  draw(x, y);
-  return false;
-}
+  canvas.onmousemove = function (e) {
+    if (!mouseIsDown) return;
+    var rect = canvas.getBoundingClientRect();
+    var x = (e.pageX - window.pageXOffset - rect.x) / 20;
+    var y = (e.pageY - window.pageYOffset - rect.y) / 20;
+    draw(x, y);
+    return false;
+  }
 
-function mouseClick(e) {
-  // click action
-}
+  function mouseClick(e) {
+    // click action
+  }
 
-function clear(ctx) {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-}
+  function clear(ctx) {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'white';
+  }
 
 
-reset.onclick = function () { clear(ctx) };
-classify.onclick = function () { console.log("classify!") };
+  reset.onclick = function () { clear(ctx) };
+  classify.onclick = function () {
+    var data = canvas.toDataURL("image/jpeg");
+    $.post(
+      '/predict',
+      data
+    )
+  };
+
+  clear(ctx);
+})
